df663505fea292b7e690a35a580e3e8b975ae941
dervisevic.amir <dervisevic.amir@gmail.com>
2023-12-20 20:15:14 +0100

    add: res.status handlers

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# Install and import request module 
+import requests
+import getpass
+
+api_login = "https://pythontest2021.azurewebsites.net/api/Login"
+api_mfa = "https://pythontest2021.azurewebsites.net/api/Get2FACode"
+
+
+user = str(input("Enter your username: "))
+pwd = getpass.getpass("Enter your password: ")
+data = {"username": user, "password": pwd, "code": "string"}
+
+res = requests.post(api_login, json=data)
+#print(res.json())
+
+# Check if the request was successful (status code 200)
+if res.status_code == 200:
+    # Parse the JSON content of the response
+    data = res.json()
+    print(data)
+
+    if data["success"] and data["is2FAEnabled"] == False:
+        print("Token: %s" % data["token"])
+
+elif res.status_code == 404:
+    print("Username or password is incorrect. Exiting.")
+    exit()
 
